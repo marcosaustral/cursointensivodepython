f77e6530d8ec98f871ecfbb63b2b78d90c3f3a35
Marcos <38471074+marcosbackend@users.noreply.github.com>
2018-05-15 16:22:39 -0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,4 +1,15 @@
 # Armazene o nome de uma pessoa em uma variável e apresente uma mensagem a essa pessoa.
-
 nome = "marcos"
 print("Alô, " + nome.title() + ", você gostaria de aprender um pouco de Python hoje?")
+
+# // outras formas de fazer que não está no livro //
+# separando strings e variaveis com uma virgula
+print("Alô,", nome.title(), "você gostaria de aprender um pouco de Python hoje?")
+
+# usando a função f"string"
+print(f"Alô, {nome}, você gostaria de aprender um pouco de Python hoje?")
+
+# usando .format(variavel)
+print("Alô, {}, você gostaria de aprender um pouco de Python hoje?".format{nome})
+
+
