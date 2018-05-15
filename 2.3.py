2a1022e5e186b5ccf7e97986a551541ca7249548
Marcos <38471074+marcosbackend@users.noreply.github.com>
2018-05-15 16:18:20 -0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,4 +1,4 @@
-# Armazene o nome de um pessoa em uma variável e apresente uma mensagem a essa pessoa.
+# Armazene o nome de uma pessoa em uma variável e apresente uma mensagem a essa pessoa.
 
 nome = "marcos"
 print("Alô, " + nome.title() + ", você gostaria de aprender um pouco de Python hoje?")
